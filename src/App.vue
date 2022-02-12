74aeb11a0ecc72351db818995be12132431ede5b
Joshua Leblanc <leblancjoshua077@gmail.com>
2022-02-12 17:49:05 -0500

    Changed main App

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,45 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <TheHeader />
+    <Home />
+    <TheFooter />
   </div>
-  <router-view />
 </template>
 
+<script>
+import TheHeader from "@/components/TheHeader";
+import Home from "@/views/Home";
+import TheFooter from "@/components/TheFooter";
+
+export default {
+  name: "App",
+  components: {
+    TheFooter,
+    Home,
+    TheHeader,
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import "stylesheets/phaistos.scss";
+@import url("https://fonts.googleapis.com/css2?family=Caudex:ital,wght@0,400;0,700;1,400;1,700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Caudex:ital,wght@0,400;0,700;1,400;1,700&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap");
+
+html,
+body {
+  margin: 0;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+a,
+span {
+  color: $dark-liver;
 }
 </style>
