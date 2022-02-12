39e8999aa807829ed7f8195648422d50897f6dd8
Joshua Leblanc <leblancjoshua077@gmail.com>
2022-02-12 17:44:21 -0500

    Created Separator component

---
+++ src/components/Separator.vue
@@ -0,0 +1,31 @@
+<template>
+  <hr :style="separatorStyle" />
+</template>
+
+<script>
+export default {
+  name: "Separator",
+  props: {
+    separatorHeight: Number,
+    separatorWidth: Number,
+  },
+  computed: {
+    separatorStyle() {
+      return {
+        height: `${this.separatorHeight}px`,
+        width: `${this.separatorWidth}%`,
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "/src/stylesheets/phaistos.scss";
+
+hr {
+  color: $white;
+  background-color: $white;
+  border-color: $white;
+}
+</style>
