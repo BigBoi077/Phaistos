38d677371114d921c3c3684e8f2e7f1bda7c7fe0
Joshua Leblanc <leblancjoshua077@gmail.com>
2022-02-16 11:50:38 -0500

    Created reusable link button component

---
+++ src/components/LinkButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <router-link :to="href">{{ title }}</router-link>
+</template>
+
+<script>
+export default {
+  name: "LinkButton",
+  props: {
+    href: String,
+    title: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "/src/stylesheets/phaistos.scss";
+
+a,
+a:hover {
+  text-decoration: none;
+}
+
+a {
+  letter-spacing: 2px;
+  font-size: 20px;
+  padding: 12px 16px;
+  background-color: $chocolate-web;
+  font-family: "Caudex", serif;
+  color: $white;
+}
+
+a:hover {
+  background-color: $flame;
+  box-shadow: 4px 4px 0 1px $dim-gray;
+}
+</style>
