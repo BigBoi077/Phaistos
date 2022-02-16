a37a3db44abd9465e33a45235982e8a74cc73b1d
Joshua Leblanc <leblancjoshua077@gmail.com>
2022-02-16 11:51:40 -0500

    Added Woodworking component to Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <Hero />
   <Introduction />
+  <Woodworking />
 </template>
 
 <script>
 import Hero from "@/components/Hero";
 import Introduction from "@/components/Introduction";
+import Woodworking from "@/components/Woodworking";
 
 export default {
   name: "Home",
   components: {
     Hero,
     Introduction,
+    Woodworking,
   },
 };
 </script>
