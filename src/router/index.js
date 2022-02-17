d2c2cd79c589b3acda94cf2b7c1bda3d627ac225
Joshua Leblanc <leblancjoshua077@gmail.com>
2022-02-16 21:46:14 -0500

    Added new routes

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,16 @@ const routes = [
     component: Home,
   },
   {
+    path: "/home",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/shop",
+    name: "Shop",
+    component: Home,
+  },
+  {
     path: "/about",
     name: "About",
     // route level code-splitting
