4b17225b7d6af3a39742b64f81f8794588e5ecf3
Joshua Leblanc <leblancjoshua077@gmail.com>
2022-02-21 13:27:39 -0500

    Created page header component

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  name: "PageHeader",
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "/src/stylesheets/phaistos.scss";
+
+h1 {
+  background-color: $persian-orange;
+  color: $white;
+  padding: 32px 60px;
+  width: 100%;
+  font-size: 38px;
+  font-family: "Caudex", serif;
+  font-weight: lighter;
+}
+</style>
