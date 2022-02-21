2dfb5d826e364e70cbd880b43b83633829624c12
Joshua Leblanc <leblancjoshua077@gmail.com>
2022-02-21 13:29:30 -0500

    Updated assets names

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -30,7 +30,7 @@ export default {
           name: "Chalet",
           description:
             "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga",
-          imageUrl: require("@/assets/birdhouse/birdhouse-1.jpg"),
+          imageUrl: require("@/assets/shop/chalet.jpg"),
           imageAlt: "vero eos et accusamus et iusto odio.",
           externalLink: "/test",
           externalLinkText: "Test",
@@ -39,7 +39,7 @@ export default {
           name: "Casa El Blanco",
           description:
             "Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. ",
-          imageUrl: require("@/assets/birdhouse/birdhouse-2.jpg"),
+          imageUrl: require("@/assets/shop/trio.jpg"),
           imageAlt: "harum quidem rerum facilis est et expedita.",
           externalLink: "/test",
           externalLinkText: "Test",
@@ -48,7 +48,7 @@ export default {
           name: "Flower",
           description:
             "Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
-          imageUrl: require("@/assets/birdhouse/birdhouse-3.jpg"),
+          imageUrl: require("@/assets/shop/flower.jpg"),
           imageAlt: "Temporibus autem quibusdam et aut officiis debitis.",
           externalLink: "/test",
           externalLinkText: "Test",
