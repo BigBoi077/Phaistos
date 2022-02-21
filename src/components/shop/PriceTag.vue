94bd91d6cb884ee57975a67bcdacfe28f302bc50
Joshua Leblanc <leblancjoshua077@gmail.com>
2022-02-21 13:28:00 -0500

    Created price tag component

---
+++ src/components/shop/PriceTag.vue
@@ -0,0 +1,30 @@
+<template>
+  <span>{{ formattedPrice }}</span>
+</template>
+
+<script>
+export default {
+  name: "PriceTag",
+  props: {
+    price: Number,
+    format: String,
+  },
+  computed: {
+    formattedPrice() {
+      return `${this.price} $`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../stylesheets/phaistos";
+
+span {
+  display: block;
+  font-family: "Lato", sans-serif;
+  font-size: 24px;
+  font-weight: lighter;
+  color: $dark-liver;
+}
+</style>
